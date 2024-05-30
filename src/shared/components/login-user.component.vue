8d701e05d98efc1a39b93af1b61bd21455bfd4b5
Rodrigo2901 <u202215489@upc.edu.pe>
2024-05-29 22:11:40 -0500

    fix(sign-in-up): corrected login validation and restructured component.

--- src/shared/components/login-user.component.vue
+++ src/shared/components/login-user.component.vue
@@ -1,8 +1,7 @@
 <script>
-
 export default {
-  name: "register-user",
-  props: { entity: null, visible: Boolean, entityName: '', edit: Boolean, size: 'default' },
+  name: "login-user",
+  props: { entity: Object, visible: Boolean, entityName: String, edit: Boolean, size: String },
   data() {
     return {
       visible: true,
@@ -13,15 +12,15 @@ export default {
     onCancel() {
       this.$emit('canceled');
     },
-    onRegister() {
+    onLogin() {
       this.submitted = true;
-      this.$emit('registerAttempted');
+      this.$emit('loginAttempted', this.entity);
     }
   },
   computed: {
     dialogClass() {
       return {
-        'register-dialog-visible': this.visible
+        'login-dialog-visible': this.visible
       }
     }
   }
@@ -29,26 +28,36 @@ export default {
 </script>
 
 <template>
-  <pv-dialog v-bind:visible="visible" :modal="true" class="register-dialog" :header="entityName" :class="dialogClass" :closable="false" @cancel="onCancel">
+  <pv-dialog v-bind:visible="visible" :modal="true" class="login-dialog" :header="entityName" :class="dialogClass"
+             :closable="false" @cancel="onCancel">
     <template #header>
-      <h1 style="text-align: center; padding-bottom: 10px">Sign Up</h1>
+      <h1 style="text-align: center; padding-bottom: 10px">Sign In</h1>
     </template>
     <slot name="content"></slot>
     <template #footer>
-      <div class="register-buttons">
+      <div class="login-buttons">
         <div>
-          <pv-button class="login-button" label="Sign Up" @click="onRegister"></pv-button>
+          <pv-button class="login-button" label="Sign In" @click="onLogin"></pv-button>
         </div>
-        <div><button class="google-button">
-          <img src="/src/assets/icon-google.png" alt="Google" class="google-icon"> Sign In with Google
-        </button></div>
+        <div>
+          <button class="google-button">
+            <img src="/src/assets/icon-google.png" alt="Google" class="google-icon"> Sign In with Google
+          </button>
+        </div>
+      </div>
+      <div class="login-footer">
+        <router-link to="/forgot-password" style="color: #81D440">Forgot Password?</router-link>
+        <div>
+          <span>Don't have an account? </span>
+          <router-link to="/register" style="color: #81D440">Sign Up</router-link>
+        </div>
       </div>
     </template>
   </pv-dialog>
 </template>
 
 <style>
-.register-dialog {
+.login-dialog {
   min-width: 500px;
   max-width: 90vw;
   height: auto;
@@ -61,13 +70,13 @@ export default {
   font-size: 24px;
 }
 
-.register-dialog-visible {
+.login-dialog-visible {
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
 }
 
-.register-buttons {
+.login-buttons {
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -96,7 +105,7 @@ export default {
 }
 
 .google-button {
-  width: 500px;
+  width: 100%;
   height: 70px;
   display: flex;
   flex-direction: row;
@@ -126,5 +135,19 @@ export default {
   margin-right: 12px;
   margin-bottom: 4px;
   flex-shrink: 0;
+}
+
+.login-footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  margin-top: 20px;
+  gap: 10px;
+}
+
+.login-footer a {
+  text-decoration: none;
 }
 </style>
